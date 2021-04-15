21fd1ac5ec20938a33d06c55f69da66e73f3f1d5
Ronald Santos <ronaldpsantos@outlook.com>
2021-04-15 08:54:35 -0300

    Angular Model

--- QuickBuy.Web/ClientApp/src/app/User/Login/login.component.ts
+++ QuickBuy.Web/ClientApp/src/app/User/Login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { User } from "../../model/User";
 
 @Component({
   selector: "app-login",
@@ -6,11 +7,15 @@ import { Component } from "@angular/core";
   styleUrls: ["./login.component.css"]
 })
 export class LoginComponent {
+
+  public user;
+
+  constructor() {
+    this.user = new User();
+  }
+  
   entrar() {
-    console.log("Entrar no sistema");
+    
   }
 
-  on_keypress() {
-    console.log("FieldEmail");
-  }
 }
