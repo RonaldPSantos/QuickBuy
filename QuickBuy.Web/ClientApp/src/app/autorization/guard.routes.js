e4fe5b4f10be04967dfa9fca0e2c93403a7b0076
Ronald Santos <ronaldpsantos@outlook.com>
2021-04-19 08:34:39 -0300

    Inicio Implementação Guarda rotas

---
+++ QuickBuy.Web/ClientApp/src/app/autorization/guard.routes.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var GuardRoutes = /** @class */ (function () {
+    function GuardRoutes() {
+    }
+    GuardRoutes.prototype.canActivate = function (route, state) {
+        return true;
+    };
+    return GuardRoutes;
+}());
+exports.GuardRoutes = GuardRoutes;
+//# sourceMappingURL=guard.routes.js.map
